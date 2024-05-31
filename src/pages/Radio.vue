6ac84a2a50aa0371e60dafc1995312fd46c47ef9
kdk1026 <kdk1026@gmail.com>
2024-06-01 00:37:31 +0900

    radio에 name 추가

--- src/pages/Radio.vue
+++ src/pages/Radio.vue
@@ -2,10 +2,10 @@
     <div>
         <div>Picked: {{checkedGender}}</div>
 
-        <input type="radio" id="male" value="male" v-model="checkedGender" />
+        <input type="radio" id="male" name="gender" value="male" v-model="checkedGender" />
         <label for="male">남자</label>
 
-        <input type="radio" id="female" value="female" v-model="checkedGender" />
+        <input type="radio" id="female" name="gender" value="female" v-model="checkedGender" />
         <label for="female">여자</label>
 
     </div>
